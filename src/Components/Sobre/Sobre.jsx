5b69d2c8266fc9a168429e4ec5dcf1ca92a0e9bb
Fernanda <febeltraoleite@gmail.com>
2024-11-24 04:28:52 -0300

    Feat : Adicione os Componentes da página Sobre

--- src/Components/Sobre/Sobre.jsx
+++ src/Components/Sobre/Sobre.jsx
@@ -4,24 +4,24 @@ import {
 } from "./Style";
 import AntigoStarbucks from "/StarbucksAntigo.png";
 
-
-
-export default function Home(){
-    return(
+export default function Home() {
+    return (
         <main>
             <StyleImagem>
-                <img src={AntigoStarbucks} alt="" />
+                <img src={AntigoStarbucks} alt="imagem de um antigo Starbucks" />
             </StyleImagem>
 
-            <StylePrimeiraSessao>
-                <h2>PREPARAÇÃO</h2>
-                <h1>Níveis de torra</h1>
+            <section>
+                <StylePrimeiraSessao>
+                    <h2>PREPARAÇÃO</h2>
+                    <h1>Níveis de torra</h1>
                     <p>
                         Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou Torra Escura? Estas são as torras que fazem parte dos níveis de torra Starbucks®.
                     </p>
 
-                    <button>Saiba mais</button>
-            </StylePrimeiraSessao>
+                    <button aria-label="Saiba mais">Saiba mais</button>
+                </StylePrimeiraSessao>
+            </section>
         </main>
     );
 }
