4dd2faf0ebb797caf0abfac64f082be09f912b33
Fernanda <febeltraoleite@gmail.com>
2024-11-24 03:57:31 -0300

    Feat : Adiciona Componentes da página Novidades

--- src/Components/Novidades/Novidades.jsx
+++ src/Components/Novidades/Novidades.jsx
@@ -1,26 +1,24 @@
-import {} from "./Style";
+import { StyleMain, StyleContainer, StyledSegundaSessao } from "./Style";
 import Grao from "/grao.png";
 
+export default function Home() {
+    return (
+        <StyleMain>
+            <StyleContainer>
+                <h2>PREPARAÇÃO</h2>
+                <h1>Níveis de torra</h1>
+                <p>
+                    Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou Torra Escura? Estas são as torras que fazem parte dos níveis de torra Starbucks®.
+                </p>
 
+                <button>Saiba mais sobre os níveis de torra</button>
+            </StyleContainer>
 
-export default function Home(){
-    return(
-       <>
-         <main>
-                <section>
-                    <h2>PREPARAÇÃO</h2>
-                    <h1>Níveis de torra</h1>
-                    <p>Qual a torra que prefere? Starbucks®Torra Clara, Torra Média ou Torra Escura? Estas sãos as torras que fazem parte dos níveis de torra Starbucks®</p>
-
-                    <button>SAIBA MAIS</button>
-                </section>
-
-                <section>
-                    <figure>
-                        <img src={Grao} alt="" />
-                    </figure>
-                </section>
-            </main>
-       </>
+            <StyledSegundaSessao>
+                <figure>
+                    <img src={Grao} alt="Imagem de grãos de café representando os níveis de torra" />
+                </figure>
+            </StyledSegundaSessao>
+        </StyleMain>
     );
 }
--- src/Components/Novidades/Style.jsx
+++ src/Components/Novidades/Style.jsx
@@ -1,2 +1,70 @@
 import styled from "styled-components";
 
+const font = "'Poppins', sans-serif";
+
+export const StyleMain = styled.main`
+  background-color: #1E3932; 
+  min-height: 100vh; 
+  margin-top: 5.6rem;
+  padding: 12rem; 
+`;
+
+export const StyleContainer = styled.div`
+    
+    margin-left: 7.5rem;
+
+
+    h2 {
+    font-size: 1.5rem;
+    font-family: ${font};
+    font-weight: 600;
+    color: white;
+    
+  }
+
+  h1 {
+    font-size: 4rem;
+    font-family: ${font};
+    font-weight: 400;
+    color: white;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-size: 1.6rem;
+    width: 32.8vw;
+    color: white;
+    font-family: ${font};
+    font-weight: 300;
+    margin-top: -0.5rem;
+    margin-bottom: 2rem;
+  }
+
+  button {
+    background-color: #00754a;
+    color: white;
+    border: none;
+    border-radius: 30px;
+    padding: 0.7rem 3rem;
+    font-size: 1.2rem;
+    font-family: ${font};
+    font-weight: 600;
+    
+
+    &:hover {
+      background-color: #005b39;
+    }
+  }
+`;
+
+export const StyledSegundaSessao = styled.section`
+
+    img{
+        width: 29.8vw;
+        margin-left: 56rem;
+        margin-top: -22rem;
+        
+    
+    }
+
+`;
