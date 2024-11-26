90973448dc00422d9c5f9e9af84e3dd599407b95
Fernanda <febeltraoleite@gmail.com>
2024-11-26 18:03:12 -0300

    Feat : Adiciona os Componentes de responsividade da Página Sobre

--- src/Components/Sobre/Style.jsx
+++ src/Components/Sobre/Style.jsx
@@ -3,30 +3,34 @@ import styled from "styled-components";
 const font = "'Poppins', sans-serif";
 
 export const StyleImagem =  styled.section`
+  margin-top: 12.4rem;
+  margin-left: 30rem;
 
-    margin-top: 12.4rem;
-    margin-left: 30rem;
+  img {
+    width: 20vw;
+  }
 
-    img{
-        width: 20vw;
-        
+  @media (max-width: 600px) {
+    margin-top: 1em;
+    margin-left: 10vw;
+    
+    img {
+      width: 45vw;
+     
     }
+  }
 `;
 
 export const StylePrimeiraSessao = styled.section`
+  position: relative;
+  left: 63rem;
+  top: -28rem;
 
-    position: relative;
-    left: 63rem;
-    top: -28rem;
-    
-
-
-    h2 {
+  h2 {
     font-size: 1.2rem;
     font-family: ${font};
     font-weight: 600;
     color: black;
-    
   }
 
   h1 {
@@ -56,12 +60,44 @@ export const StylePrimeiraSessao = styled.section`
     font-size: 1rem;
     font-family: ${font};
     font-weight: 600;
-    
 
     &:hover {
       background-color: #005b39;
     }
   }
 
+  @media (max-width: 600px) {
+    left: 6vw;
+    top: 0; 
 
+    h2 {
+      font-size: 0.9rem; 
+      margin-top: 0.5rem;
+      margin-left: 15rem;
+      margin-bottom: -0.6rem;
+    }
+
+    h1 {
+      font-size: 2.2rem; 
+      margin-left: 5.5rem;
+      margin-bottom: 0.9rem; 
+      font-weight: 400;
+    }
+
+    p {
+      font-size: 0.9rem; 
+      width: 70%; 
+      margin-left: 5.6rem;
+      margin-bottom: 0.4rem;
+      
+    }
+
+    button {
+      width: 35%; 
+      font-size: 0.8rem; 
+      padding: 0.6rem 1.2em; 
+      margin-left: 5rem;
+      
+    }
+  }
 `;
